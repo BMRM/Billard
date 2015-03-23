c39f0d26fd1cba876366127cb27d873e384cc7e0
Romain Mekarni <romain.mekarni@gmail.com>
2015-03-23 22:37:37 +0100

    collision working

--- Billard.java
+++ Billard.java
@@ -18,8 +18,8 @@ public class Billard
 
 	static class Vector
 	{
-		double x, y;
-		double module;
+		double x;
+        double y;
 	}
 
 	static class Color
@@ -29,15 +29,16 @@ public class Billard
 
 	static class Ball
 	{
-		double rayon;
-		double x, y;
-		Vector vitesse = new Vector();
-		double weight;
+		double r;
+		double x;
+        double y;
+		Vector v = new Vector();
+		double m;
 	}
 
 	static class RenderBall
 	{
-		int rayon;
+		int r;
 		int x, y;
 		Color couleur = new Color();
 	}
@@ -52,58 +53,40 @@ public class Billard
 		EcranGraphique.fillRect(10, 10, length, width);
 	}
 
-	static void calcPosLine(Box box, Ball [] balls, int n)
+	static void calcPosLine(Ball [] balls, int n)
 	{
 		for (int i = 0; i <= n - 1; i++)
 		{
-			balls[i].x = 0.3 + (box.length/(n+1)) * i;
-			balls[i].y = box.width/2;
+			balls[i].x = 0.3 + (Box.length/(n+1)) * i;
+			balls[i].y = Box.width/2;
 		}
 	}
 
-	static void calcPosTriangle(Box box, Ball [] balls , int k)
+	static void calcPosTriangle(Ball [] balls , int k)
 	{
 		double decal = 0;
 		int b = 0;
 		int n = (k * (k+1)/2) + 1;
-		balls[n - 1].x = box.length/3;
-		balls[n - 1].y = box.width/2;
+		balls[n - 1].x = Box.length/3;
+		balls[n - 1].y = Box.width/2;
 		for (int i = 1; i <= k; i++)
 		{
 
 			for (int j = 0; j <= i - 1; j++)
 			{
-				balls[b].x = (2 * box.length / 3) + decal;
-				balls[b].y = (box.width/2) + (2 * (balls[b].rayon + 0.01) * j) - ((decal)/2);
+				balls[b].x = (2 * Box.length / 3) + decal;
+				balls[b].y = (Box.width/2) + (2 * (balls[b].r + 0.01) * j) - ((decal)/2);
 				b++;
 			}
 
-			decal += (2 * balls[i].rayon);
+			decal += (2 * balls[i].r);
 		}
 	}
 
-    static void formeTrigo(Vector v)
+    static double module(Vector v)
     {
-        v.module = Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
-        if (v.module != 0)
-        {
-            v.x /= v.module;
-            v.y /= v.module;
-        }
+        return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
     }
-
-    static void evolve(Ball ball, double dt)
-    {
-        ball.x += dt * ball.vitesse.module * ball.vitesse.x;
-        ball.y += dt * ball.vitesse.module * ball.vitesse.y;
-        //ball.vitesse.module *= 0.99;
-    }
-
-	static void evolve(Box box,Ball [] balls, int n, double dt)
-	{
-		for (int i = 0; i < n; i++)
-            evolve(balls[i], dt);
-	}
 
 	static void render(RenderBall[]renderballs, Ball []balls, int n)
 	{
@@ -113,53 +96,12 @@ public class Billard
 			renderballs[i].x = (int)(balls[i].x * scale) + 10;
 			renderballs[i].y = (int)(balls[i].y * scale) + 10;
 			EcranGraphique.setColor(renderballs[i].couleur.r, renderballs[i].couleur.v, renderballs[i].couleur.b);
-			EcranGraphique.fillCircle(renderballs[i].x, renderballs[i].y, renderballs[i].rayon);
+			EcranGraphique.fillCircle(renderballs[i].x, renderballs[i].y, renderballs[i].r);
 		}
 		EcranGraphique.flush();
 	}
 
-	static boolean chocBox(Ball ball, double dt)
-	{
-        double t;
-        double vx = ball.vitesse.module * ball.vitesse.x;
-        double vy = ball.vitesse.module * ball.vitesse.y;
-
-        t = (ball.rayon - ball.x) / vx;
-        if (t >= 0 && t < dt)
-        {
-            evolve(ball, t);
-            ball.vitesse.x *= -1;
-            evolve(ball, dt - t);
-            return true;
-        }
-        t = (Box.length - ball.rayon - ball.x) / vx;
-        if (t >= 0 && t < dt)
-        {
-            evolve(ball, t);
-            ball.vitesse.x *= -1;
-            evolve(ball, dt - t);
-            return true;
-        }
-        t = (ball.rayon - ball.y) / vy;
-        if (t >= 0 && t < dt)
-        {
-            evolve(ball, t);
-            ball.vitesse.y *= -1;
-            evolve(ball, dt - t);
-            return true;
-        }
-        t = (box.width - ball.rayon - ball.y) / vy;
-        if (t >= 0 && t < dt)
-        {
-            evolve(ball, t);
-            ball.vitesse.y *= -1;
-            evolve(ball, dt - t);
-            return true;
-        }
-        return false;
-	}
-
-    static double evolve(Ball[] balls, int n, double dt)
+    static double update(Ball[] balls, int n, double dt)
     {
         double t = dt;
         double tmp;
@@ -170,7 +112,7 @@ public class Billard
         {
             for (int j = i + 1; j < n; j++)
             {
-                tmp = dtChocBall(balls[i], balls[k], dt);
+                tmp = dtChocBalls(balls[i], balls[j], dt);
                 if (tmp != -1 && tmp < t)
                 {
                     // Get the first choc
@@ -179,7 +121,7 @@ public class Billard
                     l = j;
                 }
             }
-            tmp = dtChocBox(ball[i], t);
+            tmp = dtChocBox(balls[i], dt);
             if (tmp != -1 && tmp < t)
             {
                 k = i;
@@ -189,14 +131,12 @@ public class Billard
         }
         if (t < dt)
         {
-            evolve(balls, t);
+            evolve(balls, n, t);
             if (l > 0)
-            {
-                (balls, n, t);
-                chocElastic(balls[k], balls[j]);
-            }
+                chocBalls(balls[k], balls[l]);
             else
                 chocBox(balls[k]);
+            evolve(balls, n, 0.00001);
             return t;
         }
         else
@@ -204,31 +144,48 @@ public class Billard
         return dt;
     }
 
-    static void chocElastic(Ball b1, Ball b2)
+    static void evolve(Ball b, double dt)
     {
-        double mu = b1.weight / b2.weight;
-        double cos = (b1.x - b2.x) / (b1.rayon + b2.rayon);
-        double sin = (b1.y - b2.y) / (b1.rayon + b2.rayon);
-
-        b1.vitesse.x = b1.vitesse.module * (mu - 1 + 2 * Math.pow(cos, 2)) / (mu + 1);
-        b1.vitesse.y = b1.vitesse.module * (2 * sin * cos) / (mu + 1);
-        b2.vitesse.x = b2.vitesse.module * (2 * mu * Math.pow(sin, 2)) / (mu + 1);
-        b2.vitesse.y = b2.vitesse.module * (-2 * mu * sin * cos) / (mu + 1);
-        formeTrigo(b1.vitesse);
-        formeTrigo(b2.vitesse);
+        b.x += dt * b.v.x;
+        b.y += dt * b.v.y;
+        //ball.v.module *= 0.99;
     }
 
-    static double dtChocBall(Ball b1, Ball b2, double dt)
+    static void evolve(Ball [] balls, int n, double dt)
+    {
+        for (int i = 0; i < n; i++)
+            evolve(balls[i], dt);
+    }
+
+    static double dtChocBox(Ball b, double dt)
+    {
+        double t = dt;
+        double tmp;
+
+        tmp = (b.r - b.x) / b.v.x;
+        if (tmp >= 0 && tmp < t)
+            t = tmp;
+        tmp = (Box.length - b.r - b.x) / b.v.x;
+        if (tmp >= 0 && tmp < t)
+            t = tmp;
+        tmp = (b.r - b.y) / b.v.y;
+        if (tmp >= 0 && tmp < t)
+            t = tmp;
+        tmp = (Box.width - b.r - b.y) / b.v.y;
+        if (tmp >= 0 && tmp < t)
+            t = tmp;
+        if (t < dt)
+            return t;
+        return -1;
+    }
+
+    static double dtChocBalls(Ball b1, Ball b2, double dt)
     {
         // Choc equation : a * dt * dt + b * dt + c = 0
         double t;
-        Vector v1 = b1.vitesse;
-        Vector v2 = b2.vitesse;
-        double dvx = v1.module * v1.x - v2.module * v2.x;
-        double dvy = v1.module * v1.y - v2.module * v2.y;
-        double b = 2 * dvx * (b1.x - b2.x) + 2 * dvy * (b1.y - b2.y);
-        double a = Math.pow(dvx, 2) + Math.pow(dvy, 2);
-        double c = Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2) - Math.pow(b1.rayon + b2.rayon, 2);
+        double b = 2 * (b1.v.x - b2.v.x) * (b1.x - b2.x) + 2 * (b1.v.y - b2.v.y) * (b1.y - b2.y);
+        double a = Math.pow(b1.v.x - b2.v.x, 2) + Math.pow(b1.v.y - b2.v.y, 2);
+        double c = Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2) - Math.pow(b1.r + b2.r, 2);
         double delta = Math.pow(b, 2) - 4 * a * c;
 
         if (delta > 0)
@@ -237,9 +194,52 @@ public class Billard
             t = (-b) / (2 * a);
         else
             return -1;
-        if (t < 0 || t >= dt)
-            return -1;
-        return t;
+        if (t >= 0 && t < dt)
+            return t;
+        return -1;
+    }
+
+    static void chocBox(Ball b)
+    {
+        Ecran.afficher("ChocBox\n");
+        if (b.x - b.r == 0)
+            b.v.x *= -1;
+        else if (b.x + b.r == Box.length)
+            b.v.x *= -1;
+        else if (b.y - b.r == 0)
+            b.v.y *= -1;
+        else if (b.y + b.r == Box.width)
+            b.v.y *= -1;
+    }
+
+    static void chocBalls(Ball b1, Ball b2)
+    {
+        Ecran.afficher("ChocBall\n");
+        if (module(b1.v) < module(b2.v))
+        {
+            Ball tmp = b1;
+            b1 = b2;
+            b2 = tmp;
+        }
+        Ecran.afficher("avant -> v1.x : ", b1.v.x, ", v2.x : ", b2.v.x, "\n");
+
+        double vgx = (b1.m * b1.v.x + b2.m * b2.v.x) / (b1.m + b2.m);
+        double vgy = (b1.m * b1.v.y + b2.m * b2.v.y) / (b1.m + b2.m);
+       Ecran.afficher("centre gravite v : ", 2 * vgx - b2.v.x, "\n");
+        b1.v.x = 2 * vgx - b1.v.x;
+        b1.v.y = 2 * vgy - b1.v.y;
+        b2.v.x = 2 * vgx - b2.v.x;
+        b2.v.y = 2 * vgy - b2.v.y;
+
+        /*double mu = b1.m / b2.m;
+        double cos = (b1.x - b2.x) / (b1.r + b2.r);
+        double sin = (b1.y - b2.y) / (b1.r + b2.r);
+        EcranGraphique.wait(1000);
+        b2.v.x = b1.v.module * (mu - 1 + 2 * Math.pow(cos, 2)) / (mu + 1);
+        b2.v.y = b1.v.module * (2 * sin * cos) / (mu + 1);
+        b1.v.x = b1.v.module * (2 * mu * Math.pow(sin, 2)) / (mu + 1);
+        b1.v.y = b1.v.module * (-2 * mu * sin * cos) / (mu + 1);*/
+        Ecran.afficher("apres -> v1.x : ", b1.v.x, ", v2.x : ", b2.v.x, "\n");
     }
 
 // ---------------------------------------------------------------------- Main ----------------------------------------
@@ -249,7 +249,7 @@ public class Billard
 	{
 
 		//taille de la base du triangle pour un positionnement classique
-		int k = 1;
+		int k = 5;
 
 		//nombre de boule totale pour positionnement classique
 		int n = (k * (k+1)/2) + 1;
@@ -260,49 +260,45 @@ public class Billard
 		//inititalisation des tableau de boule
 		Ball [] balls = new Ball[n + 1];
 		RenderBall [] renderballs = new RenderBall[n + 1];
-		Box box = new Box();
 
 		for(int i = 0; i < n ; i++)
 		{
 			//Def du tableau de position
 			balls[i] = new Ball();
-			balls[i].rayon = 0.03;
-			balls[i].vitesse.x = 0;// (Math.random() - 0.5) * 10;
-			balls[i].vitesse.y = 0; //(Math.random() - 0.5) * 10;
-            balls[i].vitesse.module = Math.sqrt(Math.pow(balls[i].x, 2) + Math.pow(balls[i].y, 2));
-            balls[i].x /= balls[i].vitesse.module;
-            balls[i].y /= balls[i].vitesse.module;
-			balls[i].vitesse.module = 0.03;
-			balls[i].weight = 1;
+			balls[i].r = 0.03;
+			balls[i].v.x = (Math.random() - 0.5) * 0.1;
+			balls[i].v.y = (Math.random() - 0.5) * 0.1;
+			balls[i].m = 1;
 
 			//Def du tableau de rendu
 			renderballs[i] = new RenderBall();
-			renderballs[i].rayon = (int)(balls[i].rayon * scale);
+			renderballs[i].r = (int)(balls[i].r * scale);
 			renderballs[i].couleur.r = ((int)(Math.random() * 250));
 			renderballs[i].couleur.v = ((int)(Math.random() * 250));
 			renderballs[i].couleur.b = ((int)(Math.random() * 250));
 
             if (i == n - 1)
             {
-                balls[i].vitesse.x = 1;
-                balls[i].vitesse.y = 0;
+                balls[i].v.x = 0.1;
+                balls[i].v.y = 0;
             }
 		}
 
 		EcranGraphique.init(50,50,length+100, width+100, length + 20, width + 20, "billard");
 		iniWin();
-		calcPosTriangle(box, balls , k);
-		//calcPosLine(box, balls , nline);
+		calcPosTriangle(balls , k);
+		//calcPosLine(balls , nline);
 		render(renderballs, balls, n);
 		EcranGraphique.flush();
 
+        double t;
 		while(true)
 		{
             t = 0;
             while (t < dt)
-			    t = evolve(box, balls, n, dt);
+			    t += update(balls, n, dt - t);
 			render(renderballs, balls,n);
-			EcranGraphique.wait(5);
+			EcranGraphique.wait(2);
 		}
 
 	}
