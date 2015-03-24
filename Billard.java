c98f559d0f26516ea3a540306330f1142ff9ea91
Romain Mekarni <romain.mekarni@gmail.com>
2015-03-24 23:33:11 +0100

    GRR

--- Billard.java
+++ Billard.java
@@ -30,8 +30,7 @@ public class Billard
 	static class Ball
 	{
 		double r;
-		double x;
-        double y;
+		Vector p = new Vector();
 		Vector v = new Vector();
 		double m;
 	}
@@ -57,8 +56,8 @@ public class Billard
 	{
 		for (int i = 0; i <= n - 1; i++)
 		{
-			balls[i].x = (Box.length/(n+1)) * i;
-			balls[i].y = Box.width/2;
+			balls[i].p.x = (Box.length/(n+1)) * i;
+			balls[i].p.y = Box.width/2;
 		}
 	}
 
@@ -67,15 +66,15 @@ public class Billard
 		double decal = 0;
 		int b = 0;
 		int n = (k * (k+1)/2) + 1;
-		balls[n - 1].x = Box.length/3;
-		balls[n - 1].y = Box.width/2;
+		balls[n - 1].p.x = Box.length/3;
+		balls[n - 1].p.y = Box.width/2 - 0.02;
 		for (int i = 1; i <= k; i++)
 		{
 
 			for (int j = 0; j <= i - 1; j++)
 			{
-				balls[b].x = (2 * Box.length / 3) + decal;
-				balls[b].y = (Box.width/2) + (2 * (balls[b].r) * j) - ((decal)/2);
+				balls[b].p.x = (2 * Box.length / 3) + decal;
+				balls[b].p.y = (Box.width/2) + (2 * (balls[b].r) * j) - ((decal)/2);
 				b++;
 			}
 
@@ -88,13 +87,30 @@ public class Billard
         return Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
     }
 
+    static Vector diff(Vector v1, Vector v2)
+    {
+        Vector r = new Vector();
+        r.x = v1.x - v2.x;
+        r.y = v1.y - v2.y;
+        return r;
+    }
+
+    static double tan(double x)
+    {
+        if (x < -Math.PI / 2)
+            x = -Math.PI / 2;
+        else if (x > Math.PI / 2)
+            x = Math.PI / 2;
+        return Math.tan(x);
+    }
+
 	static void render(RenderBall[]renderballs, Ball []balls, int n)
 	{
 		iniWin();
 		for (int i = 0; i < n; i++)
 		{
-			renderballs[i].x = (int)(balls[i].x * scale) + 10;
-			renderballs[i].y = (int)(balls[i].y * scale) + 10;
+			renderballs[i].x = (int)(balls[i].p.x * scale) + 10;
+			renderballs[i].y = (int)(-balls[i].p.y * scale) + 10 + width;
 			EcranGraphique.setColor(renderballs[i].couleur.r, renderballs[i].couleur.v, renderballs[i].couleur.b);
 			EcranGraphique.fillCircle(renderballs[i].x, renderballs[i].y, renderballs[i].r);
 		}
@@ -146,8 +162,8 @@ public class Billard
 
     static void evolve(Ball b, double dt)
     {
-        b.x += dt * b.v.x;
-        b.y += dt * b.v.y;
+        b.p.x += dt * b.v.x;
+        b.p.y += dt * b.v.y;
         //ball.v.module *= 0.99;
     }
 
@@ -162,16 +178,16 @@ public class Billard
         double t = dt;
         double tmp;
 
-        tmp = (b.r - b.x) / b.v.x;
+        tmp = (b.r - b.p.x) / b.v.x;
         if (tmp >= 0 && tmp < t)
             t = tmp;
-        tmp = (Box.length - b.r - b.x) / b.v.x;
+        tmp = (Box.length - b.r - b.p.x) / b.v.x;
         if (tmp >= 0 && tmp < t)
             t = tmp;
-        tmp = (b.r - b.y) / b.v.y;
+        tmp = (b.r - b.p.y) / b.v.y;
         if (tmp >= 0 && tmp < t)
             t = tmp;
-        tmp = (Box.width - b.r - b.y) / b.v.y;
+        tmp = (Box.width - b.r - b.p.y) / b.v.y;
         if (tmp >= 0 && tmp < t)
             t = tmp;
         if (t < dt)
@@ -183,9 +199,9 @@ public class Billard
     {
         // Choc equation : a * dt * dt + b * dt + c = 0
         double t;
-        double b = 2 * (b1.v.x - b2.v.x) * (b1.x - b2.x) + 2 * (b1.v.y - b2.v.y) * (b1.y - b2.y);
+        double b = 2 * (b1.v.x - b2.v.x) * (b1.p.x - b2.p.x) + 2 * (b1.v.y - b2.v.y) * (b1.p.y - b2.p.y);
         double a = Math.pow(b1.v.x - b2.v.x, 2) + Math.pow(b1.v.y - b2.v.y, 2);
-        double c = Math.pow(b1.x - b2.x, 2) + Math.pow(b1.y - b2.y, 2) - Math.pow(b1.r + b2.r, 2);
+        double c = Math.pow(b1.p.x - b2.p.x, 2) + Math.pow(b1.p.y - b2.p.y, 2) - Math.pow(b1.r + b2.r, 2);
         double delta = Math.pow(b, 2) - 4 * a * c;
 
         if (delta > 0)
@@ -202,13 +218,13 @@ public class Billard
     static void chocBox(Ball b)
     {
         Ecran.afficher("ChocBox\n");
-        if (b.x - b.r == 0)
+        if (b.p.x - b.r == 0)
             b.v.x *= -1;
-        else if (b.x + b.r == Box.length)
+        else if (b.p.x + b.r == Box.length)
             b.v.x *= -1;
-        else if (b.y - b.r == 0)
+        else if (b.p.y - b.r == 0)
             b.v.y *= -1;
-        else if (b.y + b.r == Box.width)
+        else if (b.p.y + b.r == Box.width)
             b.v.y *= -1;
     }
 
@@ -223,23 +239,37 @@ public class Billard
         }
         Ecran.afficher("avant -> v1.x : ", b1.v.x, ", v2.x : ", b2.v.x, "\n");
 
-        double m1 = (b1.m - b2.m) / (b1.m + b2.m);
-        double m2 = (2 * b1.m) / (b1.m + b2.m);
-        double alpha = Math.atan((b1.x - b2.x) / (b1.y - b2.y));
-        double a1 = Math.atan(b1.v.x / b1.v.y) + alpha;
-        double a2 = Math.atan(b2.v.x / b2.v.y) + alpha;
+        double v1 = module(b1.v);
+        double v2 = module(b2.v);
+        double m1 = b1.m;
+        double m2 = b2.m;
 
-        double th1 = Math.atan(m1 * Math.tan(a1) + m2 * b2.v.y / b1.v.x);
-        double th2 = Math.atan(-m1 * Math.tan(a2) + m2 * b1.v.y / b2.v.x);
+        double alpha = Math.atan((b1.p.y - b2.p.y) / (b1.p.x - b2.p.x)) - Math.PI / 2;
+        Ecran.afficher("tan a = ", (b1.p.y - b2.p.y) / (b1.p.x - b2.p.x), "\n");
+        double a1 = (b1.v.x == 0) ? -alpha : Math.atan(b1.v.y / b1.v.x) - alpha;
+        double a2 = (b2.v.x == 0) ? -alpha : Math.atan(b2.v.y / b2.v.x) - alpha;
 
-        double v1 = Math.sqrt(Math.pow(m1 * b1.v.y + m2 * b2.y, 2) + Math.pow(b1.x, 2));
-        double v2 = Math.sqrt(Math.pow(-m1 * b2.v.y + m2 * b1.y, 2) + Math.pow(b2.x, 2));
+        double th1 = (b1.v.x == 0) ? Math.PI / 2 :
+        Math.atan((m1 - m2) / (m1 + m2) * Math.tan(a1) + ((2 * m2) / (m1 + m2)) * (v2 / v1) * (Math.sin(a2) / Math.cos(a1)));
+        double th2 = (b2.v.x == 0) ? Math.PI / 2 :
+        Math.atan((b2.m - b1.m) / (b1.m + b2.m) * Math.tan(a2) + ((2 * b1.m) / (b1.m + b2.m)) * (v1 / v2) * (Math.sin(a1) / Math.cos(a2)));
 
+        double newV1 = Math.sqrt(Math.pow((m1 - m2) / (m1 + m2) * (v1 * Math.sin(a1)) + (2 * m2) / (m1 + m2) * (v2 * Math.sin(a2)), 2) + Math.pow(v1 * Math.cos(a1), 2));
+        double newV2 = Math.sqrt(Math.pow((m2 - m1) / (m1 + m2) * (v2 * Math.sin(a2)) + (2 * m1) / (m1 + m2) * (v1 * Math.sin(a1)), 2) + Math.pow(v2 * Math.cos(a2), 2));
+        //double v1 = Math.sqrt(Math.pow(b2.v.y, 2) + Math.pow(b1.v.x, 2));
+        //double v2 = Math.sqrt(Math.pow(b1.v.y, 2) + Math.pow(b2.v.x, 2));
 
-        b1.v.x = v1 * Math.cos(th1 + alpha);
-        b1.v.y = v1 * Math.sin(th1 + alpha);
-        b2.v.x = v2 * Math.cos(th2 + alpha);
-        b2.v.y = v2 * Math.sin(th2 + alpha);
+        Ecran.afficher("alpha : ", alpha, "\n");
+        Ecran.afficher("a : ", a1, "  ", a2, "\n");
+        Ecran.afficher("th : ", th1, "  ", th2, "\n");
+        Ecran.afficher("new v1, v2 : ", newV1, "  ", newV2, "\n");
+
+        b1.v.x = newV1 * Math.cos(th1 + alpha + Math.PI);
+        b1.v.y = newV1 * Math.sin(th1 + alpha + Math.PI);
+        Ecran.afficher("th - alpha : ", th1 + alpha + Math.PI, "  ", th2 + alpha, "\n");
+        b2.v.x = newV2 * Math.cos(th2 + alpha);
+        b2.v.y = newV2 * Math.sin(th2 + alpha);
+        Ecran.afficher("---- cos : ", Math.cos(th2 + alpha), "  sin : ", Math.sin(th2 + alpha), "\n");
 
 
 
@@ -261,6 +291,7 @@ public class Billard
         b1.v.x = b1.v.module * (2 * mu * Math.pow(sin, 2)) / (mu + 1);
         b1.v.y = b1.v.module * (-2 * mu * sin * cos) / (mu + 1);*/
         Ecran.afficher("apres -> v1.x : ", b1.v.x, ", v2.x : ", b2.v.x, "\n");
+        Ecran.afficher("apres -> v1.y : ", b1.v.y, ", v2.y : ", b2.v.y, "\n");
     }
 
 // ---------------------------------------------------------------------- Main ----------------------------------------
@@ -270,7 +301,7 @@ public class Billard
 	{
 
 		//taille de la base du triangle pour un positionnement classique
-		int k = 5;
+		int k = 1;
 
 		//nombre de boule totale pour positionnement classique
 		int n = (k * (k+1)/2) + 1;
@@ -287,8 +318,8 @@ public class Billard
 			//Def du tableau de position
 			balls[i] = new Ball();
 			balls[i].r = 0.03;
-			balls[i].v.x = (Math.random() - 0.5) * 0.001;
-			balls[i].v.y = (Math.random() - 0.5) * 0.001;
+			balls[i].v.x = 0;//(Math.random() - 0.5) * 0.001;
+			balls[i].v.y = 0;//(Math.random() - 0.5) * 0.001;
 			balls[i].m = 1;
 
 			//Def du tableau de rendu
@@ -300,7 +331,7 @@ public class Billard
 
             if (i == n - 1)
             {
-                balls[i].v.x = 0.1;
+                balls[i].v.x = 0.05;
                 balls[i].v.y = 0;
             }
 		}
@@ -319,7 +350,7 @@ public class Billard
             while (t < dt)
 			    t += update(balls, n, dt - t);
 			render(renderballs, balls,n);
-			EcranGraphique.wait(2);
+			EcranGraphique.wait(10);
 		}
 
 	}
